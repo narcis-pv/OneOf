7cfc92c054bca4ad148874f4196751eccf3e62f3
Narcis Dumitrescu <narcis.dumitrescu@24software.ro>
2019-07-26 12:15:53 +0300

    Mongo and Json serializer

--- OneOf.Tests/OneOfJsonConverter.cs
+++ OneOf.Tests/OneOfJsonConverter.cs
@@ -1,71 +1,147 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Reflection;
-using Dynamitey;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using JsonConvert = Newtonsoft.Json.JsonConvert;
+using JsonReader = Newtonsoft.Json.JsonReader;
+using JsonWriter = Newtonsoft.Json.JsonWriter;
 
 namespace OneOf
 {
-    public class OneOfJsonConverterWithDeserialization : JsonConverter
+    /// <summary>
+    /// Cache of implicit cast operators for <see cref="IOneOf"/>
+    /// </summary>
+    internal static class OneOfImplicitCast
+    {
+        private static readonly ConcurrentDictionary<(string OneOfType, string TargetType), MethodInfo> ImplicitCastOperatorCache;
+
+        static OneOfImplicitCast()
+        {
+            ImplicitCastOperatorCache = new ConcurrentDictionary<(string, string), MethodInfo>();
+        }
+
+        public static MethodInfo GetImplicitCast(Type targetType, Type sourceType)
+        {
+            return ImplicitCastOperatorCache.GetOrAdd(
+                (targetType.AssemblyQualifiedName, sourceType.AssemblyQualifiedName),
+                key =>
+                    targetType.GetMethod("op_Implicit",
+                        BindingFlags.Public | BindingFlags.Static,
+                        null,
+                        new[] {sourceType},
+                        new ParameterModifier[0]));
+        }
+    }
+
+    public class OneOfJsonConverter : JsonConverter
     {
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            if (value is IOneOf)
+            var oneOfValue = ((IOneOf) value).Value;
+
+            if (oneOfValue == null)
+                throw new JsonSerializationException($"Failed to serialize path '{writer.Path}' because the {value.GetType().Name}'.{nameof(IOneOf.Value)} has unsupported null value.");
+
+            serializer.Serialize(writer, new OneOfContainer
             {
-                value = ((IOneOf) value).Value;
-            }
-            serializer.Serialize(writer, new
-            {
-                Value = value,
-                Type = value.GetType().FullName
+                Value = oneOfValue,
+                Type = oneOfValue.GetType().AssemblyQualifiedName
             });
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            dynamic valueAndType = JObject.Load(reader);
+            var jtoken = JToken.Load(reader);
 
-            var typeName = (string)valueAndType.Type;
+            if (!jtoken.HasValues)
+                throw new JsonSerializationException($"Failed to deserialize type '{objectType.FullName}' because of invalid json format: '{jtoken}' was not loaded.");
+
+            var typeName = jtoken.Value<string>(nameof(OneOfContainer.Type));
+            if (string.IsNullOrWhiteSpace(typeName))
+                throw new JsonSerializationException($"Failed to deserialize type '{objectType.FullName}' because property '{nameof(OneOfContainer.Type)}' was empty.");
+
             var type = Type.GetType(typeName);
 
-            var value = ((JToken) valueAndType.Value).ToObject(type);
+            if (type == null)
+                throw new JsonSerializationException($"Failed to deserialize type '{objectType.FullName}' because type '{typeName}' was not loaded.");
 
-            var method = objectType.GetMethod("op_Implicit", 
-                BindingFlags.Public | BindingFlags.Static, 
-                null,
-                new Type[] {type},
-                new ParameterModifier[0]);
+            var value = jtoken.Value<JToken>(nameof(OneOfContainer.Value)).ToObject(type);
 
-            return method.Invoke(null, new object[] {value});
+            return OneOfImplicitCast.GetImplicitCast(objectType, type).Invoke(null, new[] { value });
         }
 
         public override bool CanConvert(Type objectType)
         {
             return objectType.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IOneOf));
-        }       
+        }
     }
-    
-    public class OneOfJsonConverter : JsonConverter
+
+    public class OneOfContainer
     {
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        public object Value { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class OneOfBsonSerializer : SerializerBase<IOneOf>
+    {
+        private static readonly JsonSerializerSettings JsonSerializerSettings = new JsonSerializerSettings
         {
-            if (value is IOneOf)
-            {
-                value = ((IOneOf) value).Value;
-            }
-            serializer.Serialize(writer, value);
+            Converters = { new OneOfJsonConverter() }
+        };
+
+        /// <summary>
+        /// The actual type that it's being (de)serialized, and not the nominal type, which in our case is an <see cref="IOneOf"/>
+        /// This should be set by the 
+        /// </summary>
+        public Type ActualType { get; }
+
+        public OneOfBsonSerializer(Type actualType)
+        {
+            ActualType = actualType ?? throw new ArgumentNullException(nameof(actualType));
         }
 
-        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        // public methods
+        /// <summary>
+        /// Deserializes a value.
+        /// </summary>
+        /// <param name="context">The deserialization context.</param>
+        /// <param name="args">The deserialization args.</param>
+        /// <returns>A deserialized value.</returns>
+        public override IOneOf Deserialize(BsonDeserializationContext context, BsonDeserializationArgs args)
         {
-            throw new NotImplementedException();
+            var serializer = BsonSerializer.LookupSerializer(typeof(BsonDocument));
+
+            var oneOfContainer = (BsonDocument)serializer.Deserialize(context, args);
+
+            var json = oneOfContainer.ToJson();
+
+            return (IOneOf)JsonConvert.DeserializeObject(json, ActualType, JsonSerializerSettings);
         }
 
-        public override bool CanConvert(Type objectType)
+        /// <summary>
+        /// Serializes a value.
+        /// </summary>
+        /// <param name="context">The serialization context.</param>
+        /// <param name="args">The serialization args.</param>
+        /// <param name="value">The object.</param>
+        public override void Serialize(BsonSerializationContext context, BsonSerializationArgs args, IOneOf value)
         {
-            return objectType.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IOneOf));
+            var bsonDocument = BsonDocument.Parse(JsonConvert.SerializeObject(value, ActualType, JsonSerializerSettings));
+
+            context.Writer.WriteRawBsonDocument(new RawBsonDocument(bsonDocument.ToBson()).Slice);
+        }
+    }
+
+    public class OneOfBsonSerializationProvider : IBsonSerializationProvider
+    {
+        public IBsonSerializer GetSerializer(Type type)
+        {
+            return type.GetTypeInfo().ImplementedInterfaces.Contains(typeof(IOneOf)) ? new OneOfBsonSerializer(type) : null;
         }
     }
 }
--- OneOf.Tests/Serialization.cs
+++ OneOf.Tests/Serialization.cs
@@ -2,45 +2,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using OneOf.Types;
 
 namespace OneOf.Tests
 {
     public class Serialization
     {
         [Test]
-        public void CanSerializeOneOfValueTransparently()
-        {
-            //Given an object with a OneOf property 
-            var x = new SomeThing()
-            {
-                Value = "A string value"
-            };
-            //When that object is serialized
-            var json = JsonConvert.SerializeObject(x, new JsonSerializerSettings()
-            {
-                Converters = {new OneOfJsonConverter()}
-            });
-
-            //Then the OneOfs underlying value should have been written
-            Assert.AreEqual("{\"Value\":\"A string value\"}", json);
-        }
-
-        [Test]
         public void CanDeserializeOneOfInherited()
         {
             //Given an object with a OneOf property 
             var x = new SomeThing()
             {
-                Value = "A string value"
+                Prop = "A string value"
             };
             var serializerSettings = new JsonSerializerSettings()
             {
-                Converters = {new OneOfJsonConverterWithDeserialization()}
+                Converters = {new OneOfJsonConverter()}
             };
             
             //When that object is serialized
@@ -48,10 +35,53 @@ namespace OneOf.Tests
 
             var deserializedX = JsonConvert.DeserializeObject<SomeThing>(json, serializerSettings);
 
-            Assert.AreEqual(x.Value, deserializedX.Value);
+            Assert.AreEqual(x.Prop, deserializedX.Prop);
 
         }
-        
+
+        [Test]
+        public void CanDeserializeOneOfDefaultProperty()
+        {
+            //Given an object with a OneOf property 
+            var x = new SomeThingNone();
+
+            var serializerSettings = new JsonSerializerSettings
+            {
+                Converters = { new OneOfJsonConverter() }
+            };
+
+            //When that object is serialized
+            var json = JsonConvert.SerializeObject(x, serializerSettings);
+
+            var deserializedX = JsonConvert.DeserializeObject<SomeThingNone>(json, serializerSettings);
+
+            Assert.AreEqual(x.Prop, deserializedX.Prop);
+        }
+
+
+        [Test]
+        public void ThrowSerializationExceptionWhenOneOfClassesHasNullValue()
+        {
+            var x = new ClassWithOneOfClasses();
+
+            var serializerSettings = new JsonSerializerSettings { Converters = { new OneOfJsonConverter() } };
+
+            Assert.Throws<JsonSerializationException>(() => JsonConvert.SerializeObject(x, serializerSettings));
+        }
+
+        [Test]
+        [TestCase("{}")]
+        [TestCase("{ \"Type\": \"\" }")]
+        [TestCase("{ \"Value\": \"Test\" }")]
+        [TestCase("{ \"Type\": null, \"Value\": null }")]
+        public void ThrowSerializationExceptionOnInvalidJsonFormat(string propJson)
+        {
+            var serializerSettings = new JsonSerializerSettings { Converters = { new OneOfJsonConverter() } };
+
+            Assert.Throws<JsonSerializationException>(() => 
+                JsonConvert.DeserializeObject<SomeThing>($"{{ \"Prop\": {propJson} }}", serializerSettings));
+        }
+          
         [Test]
         public void CanDeserializeOneOfValue()
         {
@@ -59,7 +89,7 @@ namespace OneOf.Tests
             var x = (OneOf<Class1, Class2, string>) (new Class2() {Name = "Savvas"});
             var serializerSettings = new JsonSerializerSettings()
             {
-                Converters = {new OneOfJsonConverterWithDeserialization()}
+                Converters = {new OneOfJsonConverter()}
             };
             
             //When that object is serialized
@@ -71,7 +101,31 @@ namespace OneOf.Tests
             Assert.AreEqual(x.GetType(), deserializedX.GetType());
             Assert.AreEqual(((Class2)x.Value).Name, ((Class2)deserializedX.Value).Name);
         }
+
+        [Test]
+        public void CanWriteAndReadOneOfFromMongoDb()
+        {
+            var client = new MongoClient(new MongoClientSettings { Server = new MongoServerAddress("localhost", 27017) });
+            var database = client.GetDatabase("test");
+            
+            BsonSerializer.RegisterSerializationProvider(new OneOfBsonSerializationProvider());
+            var mongoCollection = database.GetCollection<SomeThing>(nameof(SomeThing));
+
+            var prop = Guid.NewGuid().ToString();
+
+            var expected = new SomeThing { Prop = prop};
+
+            mongoCollection.InsertOne(expected);
+
+            var builder = new FilterDefinitionBuilder<SomeThing>();
+
+            var actual = mongoCollection.FindSync(builder.Eq(thing => thing.Prop, prop)).Single();
+
+            Assert.AreEqual(expected.Prop.Value, actual.Prop.Value);
+        }
     }
+
+
 
     class Class1
     {
@@ -102,14 +156,25 @@ namespace OneOf.Tests
             return (Name != null ? Name.GetHashCode() : 0);
         }
     }
-    
+
+    [BsonIgnoreExtraElements]
     class SomeThing
     {
-        public OneOf<string, SomeOtherThing> Value { get; set; }
+        public OneOf<string, SomeOtherThing> Prop { get; set; }
+    }
+
+    class SomeThingNone
+    {
+        public OneOf<None,SomeOtherThing> Prop { get; set; }
+    }
+
+    class ClassWithOneOfClasses
+    {
+        public OneOf<SomeThing, SomeOtherThing> Prop { get; set; }
     }
 
     internal class SomeOtherThing
     {
-        public int Value { get; set; }
+        public int Prop { get; set; }
     }
 }
